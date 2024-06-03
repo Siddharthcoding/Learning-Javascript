ad892f2faaa10495381d4a1deec3843cec96be57
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2024-06-03 14:42:41 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "Siddharth"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a number) still it's type is a number
+// true => 1; false => 0
+
+let isLoggedIn = "Siddharth"
+let boooleanIsLoggedIN = Boolean(isLoggedIn);
+
+console.log(boooleanIsLoggedIN);
+
+// 1 =>true; 0=> false 
+// "" =>false
+// "Siddharth" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
