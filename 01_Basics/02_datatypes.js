3e6582cf3ca44e9c8e69e9f715f674a2201aca60
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2024-06-03 14:25:27 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +
+    3) //Code readability should be high avoid doing this
+
+console.log("Siddharth")
+
+let name = "Siddharth"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
