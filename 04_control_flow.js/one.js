576fa78645293e6591d9acaf758e36e1b70d18b9
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2024-06-10 08:45:49 +0000

    if else

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,30 @@
+// if
+
+const isUserLoggedIn= true
+const temperature = 40
+
+// if ( temperature < 50 ) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+// if (score >100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);  // can't access
+
+const balance = 1000
+
+if (balance > 500) console.log("test");     // implicit scope
+
+// if (balance > 500) console.log("test"), console.log("test2");  // don't write like this
+
