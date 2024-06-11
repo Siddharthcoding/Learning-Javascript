a2a6ca22ee7bd1263f00c2fa9f3aaa53b3e3b8fe
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2024-06-11 19:12:23 +0000

    Color Changer

--- 07_Projects/1_ColorChanger/app.js
+++ 07_Projects/1_ColorChanger/app.js
@@ -20,3 +20,28 @@ buttons.forEach( function(button){
         }
     })
 } )
+
+// another method
+
+// buttons.forEach(function (button) {
+//     button.addEventListener('click', (event) => {
+//       switch (event.target.id) {
+//         case 'grey':
+//           body.style.backgroundColor = event.target.id;
+//           break;
+//         case 'white':
+//           body.style.backgroundColor = event.target.id;
+//           break;
+//         case 'blue':
+//           body.style.backgroundColor = event.target.id;
+//           break;
+//         case 'yellow':
+//           body.style.backgroundColor = event.target.id;
+//           break;
+//         default:
+//           body.style.backgroundColor = 'white';
+//           break;
+//       }
+//     });
+//   });
+  
