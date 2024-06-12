bfffa3c0048e3ad77f9d4001cf4c4d28048413a6
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2024-06-12 06:48:25 +0000

    BMI Calculator

---
+++ 07_Projects/2_BMICalculator/app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+
+// this will give empty values
+// const height = parseInt(document.querySelector('#height').value)  
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();   // to prevent default submission to server
+
+    const height = parseInt(document.querySelector('#height').value)  // converting string values to int
+    const weight = parseInt(document.querySelector('#weight').value)  
+    const results = document.querySelector('#results')
+
+    if(height === "" || height < 0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height ${height}`;
+    } else if(weight === "" || weight < 0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+
+        // show result
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+    
+
+
+})
